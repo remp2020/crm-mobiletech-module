0864eccf075e54dba38bc467741ddf2bcf476605
Dominik Skerhak <dominik.skerhak@gmail.com>
2025-03-21 11:05:55 +0100

    Remove deprecated `\Crm\ApiModule\Models\Response\XmlResponse`
    Use `\Tomaj\NetteApi\Response\XmlApiResponse` instead.
    
    Class was deprecated since version [1.0.0](https://github.com/remp2020/crm-api-module/releases/tag/1.0.0).
    
    remp/crm#2337

---
+++ src/Models/Api/XmlResponse.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Crm\MobiletechModule\Models\Api;
+
+use Crm\ApiModule\Models\Response\ApiResponseInterface;
+use Nette\Http\IRequest;
+use Nette\Http\IResponse;
+use Spatie\ArrayToXml\ArrayToXml;
+
+class XmlResponse implements ApiResponseInterface
+{
+    protected int $code;
+
+    protected $rootElement;
+
+    protected $rootElementAttributes;
+
+    protected $payload;
+
+    public function __construct(array $payload, string $rootElement, array $rootElementAttributes = [])
+    {
+        $this->payload = $payload;
+        $this->rootElement = $rootElement;
+        $this->rootElementAttributes = $rootElementAttributes;
+    }
+
+    /**
+     * @deprecated use getCode()
+     */
+    public function getHttpCode()
+    {
+        return $this->getCode();
+    }
+
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    /**
+     * @deprecated use setCode()
+     */
+    public function setHttpCode($httpCode)
+    {
+        $this->setCode($httpCode);
+    }
+
+    public function setCode(int $code)
+    {
+        $this->code = $code;
+    }
+
+    public function send(IRequest $httpRequest, IResponse $httpResponse): void
+    {
+        $httpResponse->setContentType('application/xml', 'utf-8');
+        echo ArrayToXml::convert($this->payload, [
+            'rootElementName' => $this->rootElement,
+            '_attributes' => $this->rootElementAttributes,
+        ], true, 'UTF-8');
+    }
+}
